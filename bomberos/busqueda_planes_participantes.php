0bbbbaa787572a9e3c3cc586da77c8530e95f897
Jorge Castellano <jorgecm14@gmail.com>
2015-10-02 15:59:30 -0430

    Correciones de conflictos listos

--- bomberos/busqueda_planes_participantes.php
+++ bomberos/busqueda_planes_participantes.php
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-<?php	include_once 'librerias/cabecera.php'; ?>
-	<div class="container"><br>
-
-	<?php include 'includes/conexion_bd.php';
-		extract($_POST);
-
-	$sql = "SELECT p.nomb_plan, p.desc_plan, p.cant_unid, p.tipo_plan FROM planes p, planes_responsables pr WHERE pr.cod_plan=p.cod_plan AND p.tipo_plan='$plan'";
-	$result = mysqli_query($mysqli, $sql);
-	?>		
-	<div class="row">
-		<div class="col-md-2"></div>
-		<div class="col-md-8">
-			<legend>Resultado de la búsqueda</legend>
-			<form method="POST" action="participantes_pre_inscritos.php">
-				<input type="hidden" name="plan" value="<?php echo $plan; ?>" />
-				<table class="table table-bordered">
-					<tr class="bg-warning">
-						<th class="text-center">Nombre</th>
-						<th class="text-center">Descripción</th>
-						<th class="text-center">UT</th>
-						<th class="text-center">Ver</th>
-					</tr>
-					<?php while ($consulta = mysqli_fetch_array($result)) :
-						echo "
-						<tr>
-							<td class='text-center'>$consulta[0]</td>
-							<td class='text-center'>$consulta[1]</td>
-							<td class='text-center'>$consulta[2]</td>
-							<td class='text-center' width='18%'><button type='submit' name='registro' value='$consulta[3]' class='boton-sin-estilo'>Participantes Pre-inscritos</button></td>
-						</tr>";
-					endwhile; ?>
-				</table>
-			</form>
-			<div align="center">
-				<button class="btn btn-warning" type="button" onclick=location="sala.php">Regresar a la página anterior</button>
-			</div><br>
-		</div>
-	</div>
-
-	</div>
-<?php	include_once 'librerias/pie.php'; ?>
-=======
 <?php
 	include_once 'librerias/cabecera.php'; ?>
 	<div class="container"><br>
@@ -78,11 +35,10 @@
 				</table>
 			</form>
 			<div align="center">
-				<button class="btn btn-danger" type="button" onclick=location="sala.php">Regresar a la página anterior</button>
+				<button class="btn btn-warning" type="button" onclick=location="sala.php">Regresar a la página anterior</button>
 			</div><br>
 		</div>
 	</div>
 
 	</div>
 <?php	include_once 'librerias/pie.php'; ?>
->>>>>>> 9a70ebb16401207e6c1d3f8410e000fe8d150669
